ce90eb7f70e1e0e5eb96ee2bd7ee206df0f80c7d
LIR <zenofhate@gmail.com>
2026-08-30 19:07:05 -0400

    Update BoltPage cask to v3.0.0

--- Casks/boltpage.rb
+++ Casks/boltpage.rb
@@ -1,5 +1,5 @@
 cask "boltpage" do
-  version "2.2.5"
+  version "3.0.0"
 
   name "BoltPage"
   desc "Fast, lightweight Markdown viewer and editor"
@@ -8,12 +8,12 @@ cask "boltpage" do
   # Update these URLs with your GitHub releases or hosting location
   on_arm do
     url "https://github.com/Silverfell/BoltPage/releases/download/v#{version}/BoltPage-#{version}-arm64.dmg"
-    sha256 "0a6eb743fdbec90c5de10e35829b741d444350875ad874363ce157c68ea44221"
+    sha256 "26a42ab6af7b6d877fb9a50d96fcb944d5d674bac67a0418e6f94893ba50f643"
   end
 
   on_intel do
     url "https://github.com/Silverfell/BoltPage/releases/download/v#{version}/BoltPage-#{version}-x64.dmg"
-    sha256 "487739078215583fba96715e8dd48a2b1fc793f17e625b11c8711c6d782ddcb0"
+    sha256 "5074d7df9081a3e82b346bb94189718efa18c14c22f109e491e63bee7fea9ed0"
   end
 
   auto_updates false
